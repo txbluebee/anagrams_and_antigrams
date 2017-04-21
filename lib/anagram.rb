d1b7c9309e14b995b1eb76b6a23f90c2338c19d3
Brian Fan <kuomu.fan@yahoo.com>
2017-04-21 09:48:42 -0700

    a word or phrase is an anagram, check if it's also a palindrome

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,16 @@
 class Array
-  define_method(:anagram?) do
-    new_arr = []
-    self.each() do |word|
-      word = word.downcase().split('').sort().join()
-      new_arr.push(word)
+  define_method(:anagram) do
+    letters_arr = []
+    self.each() do |sentence|
+      sentence = sentence.downcase().split('')
+      letters_arr.push(sentence)
     end
-    new_arr[0] == new_arr[1]
- end
+    first = letters_arr[0]
+    second = letters_arr[1]
+    if first.reverse().join() == second.reverse().join()
+      return 'These words are palindromes.'
+    elsif first.sort().join() == second.sort().join()
+      return 'They are anagrams.'
+    end
+  end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,9 +3,12 @@ require('anagram')
 
 describe('String#anagram') do
   it("check if two words are anagrams.") do
-    expect(["listen", "silent"].anagram?()).to(eq(true))
+    expect(["listen", "silent"].anagram()).to(eq("They are anagrams."))
   end
   it('Account for the possibility that words might have different cases but should still be anagrams') do
-    expect(["Tea", "Eat"].anagram?()).to(eq(true))
+    expect(["Tea", "Eat"].anagram()).to(eq("They are anagrams."))
+  end
+  it("If a word or phrase is an anagram, check if it's also a palindrome.") do
+    expect(["Anna", "anna"].anagram()).to(eq('These words are palindromes.'))
   end
 end
